130abbb65dbc34c8a665c3cd8f87cb7c4d113ed9
AlexandreBudan <luckynightm4re@gmail.com>
2023-11-13 22:23:22 +0100

    beginning of routing

--- src/app/routes.php
+++ src/app/routes.php
@@ -8,10 +8,21 @@ $twig = new \Twig\Environment($loader);
 
 $request = $_SERVER['REQUEST_URI'];
 session_start();
-if (isset($_SESSION['email'])) {
+if (isset($_SESSION['user'])) { // Set une session du User
     switch ($request) {
-        case '/':
-            require __DIR__ . '/../src/View/home.html';
+        case '/profil':
+            require './src/View/profil.php';
+            break;
+        case '/car':
+            require './src/View/car.php';
+            break;
+        case '/admin':
+            if ($_SESSION['user']) { //Faire le verif Admin
+                require './src/View/admin.php';
+            } else {
+                http_response_code(404);
+                echo 'Page not found';
+            }
             break;
         default:
             http_response_code(404);
@@ -21,7 +32,13 @@ if (isset($_SESSION['email'])) {
 } else {
     switch ($request) {
         case '/':
-            require __DIR__ . '/../src/View/home.html';
+            require './src/View/home.php';
+            break;
+        case '/login':
+            require './src/View/login.php';
+            break;
+        case '/register':
+            require './src/View/register.php';
             break;
         default:
             http_response_code(404);
