6665d0f169785e52679b073b0d079a0d853397bb
Alex <alexandre.budan@ynov.com>
2023-11-24 13:03:31 +0100

    avancement crud user

--- src/app/Model/UserModel.php
+++ src/app/Model/UserModel.php
@@ -17,23 +17,34 @@ class UserModel
         $this->conn = DataBase::connect();
     }
 
-    public function createUser(User $user)
+    public function createUser(User $user, Address $address)
     {
         try {
-            $stmt = $this->conn->prepare("INSERT INTO User (email, password, firstName, lastName, phone, age, gender, addressId, creationDate, newsLetter, verified, isAdmin, status) 
+            $stmtUser = $this->conn->prepare("INSERT INTO User (email, password, firstName, lastName, phone, age, gender, addressId, creationDate, newsLetter, verified, isAdmin, status) 
                                             VALUES (:email, :password, :firstName, :lastName, :phone, :age, :gender, :addressId, :creationDate, 0, 0, 0, 1)");
 
-            $stmt->bindParam(":email", $user->getEmail());
-            $stmt->bindParam(":password", password_hash($user->getPassword(), PASSWORD_DEFAULT));
-            $stmt->bindParam(":firstName", $user->getFirstName());
-            $stmt->bindParam(":lastName", $user->getLastName());
-            $stmt->bindParam(":phone", $user->getPhone());
-            $stmt->bindParam(":age", $user->getAge());
-            $stmt->bindParam(":gender", $user->getGender());
-            $stmt->bindParam(":addressId", $user->getAddressId());
-            $stmt->bindParam(":creationDate", $user->getCreationDate());
+            $stmtUser->bindParam(":email", $user->getEmail());
+            $stmtUser->bindParam(":password", password_hash($user->getPassword(), PASSWORD_DEFAULT));
+            $stmtUser->bindParam(":firstName", $user->getFirstName());
+            $stmtUser->bindParam(":lastName", $user->getLastName());
+            $stmtUser->bindParam(":phone", $user->getPhone());
+            $stmtUser->bindParam(":age", $user->getAge());
+            $stmtUser->bindParam(":gender", $user->getGender());
+            $stmtUser->bindParam(":creationDate", $user->getCreationDate());
 
-            $stmt->execute();
+            if ($address != null) {
+                $stmtUser->bindParam(":addressId", $user->getAddressId());
+                $stmtAddress = $this->conn->prepare("INSERT INTO Address (address, city, code, country, status) 
+                                            VALUES (:address, :city, :code, :country, 1)");
+                $stmtAddress->bindParam(":address", $address->getAddress());
+                $stmtAddress->bindParam(":city", $address->getCity());
+                $stmtAddress->bindParam(":code", $address->getCode());
+                $stmtAddress->bindParam(":country", $address->getCountry());
+
+                $stmtAddress->execute();
+            }
+
+            $stmtUser->execute();
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
